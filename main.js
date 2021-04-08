1828a1026a9283a666b3f21c29ea1bb1b15b5de5
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-08 14:19:39 +0200

    JS_Exercice_P3_3

---
+++ main.js
@@ -0,0 +1,11 @@
+//fonction qui change la couleur au clic de la div (id=myDivText du texte dans le html)
+//au clic d'une des div (des carré de couleur), et change le texte par la couleur de texte définit dans le html( exemple onclick="changeColor('black'))
+function changeColor(newColor) {
+   document.getElementById('myDivText').style.color = newColor;
+}
+
+
+
+
+
+
